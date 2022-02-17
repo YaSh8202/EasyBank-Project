01aa2380bb24d23b01298556454cf0155185285a
Yash <bajajy82@gmail.com>
2022-02-17 19:37:18 +0530

    Animated Hamburger Menu Added
    Added animation for overlay fade-in and fade-out on clicking on
    hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector('.overlay');
+
+
+btnHamburger.addEventListener('click',function(){
+    console.log('click hamburger');
+    if(header.classList.contains('open'))//Close hamburger menu
+    {
+        header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+    }
+    else{//OPen hamburber menu
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }
+});
