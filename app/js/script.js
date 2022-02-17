7fe8fb56454492be5f48c23690960372e064b57b
Yash <bajajy82@gmail.com>
2022-02-17 22:45:57 +0530

    Added animated mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,26 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector('body');
 const header = document.querySelector(".header");
 const overlay = document.querySelector('.overlay');
-
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click',function(){
     console.log('click hamburger');
     if(header.classList.contains('open'))//Close hamburger menu
     {
         header.classList.remove('open');
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
+        body.classList.remove('noscroll');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        })
     }
     else{//OPen hamburber menu
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        body.classList.add('noscroll');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');            
+        })
     }
 });
